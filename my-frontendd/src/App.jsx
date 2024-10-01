4fdaf402b166d14aa8789a007c633d5e2eb8d9ea
joshcyu <146075510+joshcyu@users.noreply.github.com>
2024-10-01 18:35:07 +0800

    added-front-end-files

---
+++ my-frontendd/src/App.jsx
@@ -0,0 +1,15 @@
+import ItemForm from "./ItemForm"
+import ItemList from "./ItemList"
+
+function App() {
+
+
+  return (
+    <>
+      <ItemForm />
+      <ItemList />
+    </>
+  )
+}
+
+export default App
